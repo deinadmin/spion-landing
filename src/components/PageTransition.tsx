02e2a1e3764764c5e905c725449e0c49da9706c8
Carl Steen <carldodo@mail.de>
2025-11-22 21:35:06 +0100

    Remove exit animation (instant disappearance)

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -16,7 +16,7 @@ export default function PageTransition({
         key={pathname}
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0 }}
+        exit={{ opacity: 0, transition: { duration: 0 } }}
         transition={{ ease: "easeOut", duration: 0.3 }}
         className="flex-1 flex flex-col"
       >
