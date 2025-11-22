2053285d29d0a6859484b28b6c6c74e5d7d84f20
Carl Steen <carldodo@mail.de>
2025-11-22 21:31:28 +0100

    Improve route transitions: remove exit movement, faster timing

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -16,8 +16,8 @@ export default function PageTransition({
         key={pathname}
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: 20 }}
-        transition={{ ease: "easeInOut", duration: 0.5 }}
+        exit={{ opacity: 0 }}
+        transition={{ ease: "easeOut", duration: 0.3 }}
         className="flex-1 flex flex-col"
       >
         {children}
